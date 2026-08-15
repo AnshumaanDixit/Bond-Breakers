a76ceeca274086f21cdf87f37cfd564a94287a70
Pratham Dixit <pratham@Prathams-MacBook-Air.local>
2026-08-15 23:57:28 +0530

    added alot of things such as: enemy and enemyAI, enemy will now follow and attack you when you are near them, added player's elemental attacks, use the correct element to kill the enemy otherwise you heal them, made is so its impossible to progress without killing the enemy correctly

--- entity/player.js
+++ entity/player.js
@@ -20,9 +20,35 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         this.health = 100;
         this.isStaggered = false;
         this.isAttacking = false;
+
+        this.hitbox = scene.add.rectangle(this.x,this.y,125,75,0xff0000,0);
+        scene.physics.add.existing(this.hitbox);
+        this.hitbox.body.allowGravity = false;
+        this.hitbox.body.enable = false;
+        //^^initialize a hitbox at the player's location,of width 125 and height 75, of #ff0000 color, and 0.5 opacity.
+        //^^the hitbox has no gravity and is initially disabled.
+    }
+    attack(type) {
+        if(this.isAttacking || this.isStaggered) 
+            return;
+        //^^this means if attack is triggered within 200ms of its prev activation, this means attack is being spammed.
+        this.isAttacking = true;
+        this.setVelocityX(0);
+        //^^set attacking to true and velocity to 0 to make sure the player dosent move while attacking
+
+        this.hitbox.currentElement = type;
+        this.hitbox.body.enable = true;
+        this.scene.time.delayedCall(200,()=>{
+            this.hitbox.body.enable = false;
+            this.isAttacking = false;
+        })
+        //^^the attack logic, checks for if the attack is pressed, and if it is comes here to enable hitbox, and disable it by a delay function, disabling it after 200ms
+        //^^also storing the type of attack in the hitbox itself
     }
     update() {
         this.handleMove();
+        this.handleHitbox();
+        //^^ contains the function that must be run each frame.
     }
     handleMove() {
         if(this.isStaggered || this.isAttacking)
@@ -44,5 +70,11 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         else {
 
         }
+        //^^logic for moving the player, whether by touch screen or weather by keyboard
+    }
+    handleHitbox() {
+        const offsetX = this.flipX ? -30:30;
+        this.hitbox.setPosition(this.x+offsetX,this.y+32);
+        //^^needed to keep the hitbox at player's location, this runs 60times a second to make sure the hitbox is at its place where it should be.
     }
 }
